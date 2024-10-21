0edb8872f71946e897011036f05779a0fabe08a5
Abanoub Samir <abanoubsamirshenouda@gmail.com>
2024-10-21 12:15:43 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import { FormWizard, TabContent } from "vue3-form-wizard";
+import "vue3-form-wizard/dist/style.css";
+export default {
+  components: {
+    FormWizard,
+    TabContent,
+  },
+};
+</script>
+
+<template>
+  <form-wizard>
+    <tab-content title="step1">
+      Hello
+    </tab-content>
+    <tab-content title="step1">
+      Hello
+    </tab-content>
+    <tab-content title="step1">
+      Hello
+    </tab-content>
+    <tab-content title="step1">
+      Hello
+    </tab-content>
+    <tab-content title="step1">
+      Hello
+    </tab-content>
+  </form-wizard>
+</template>
+
+<style scoped>
+
+</style>
